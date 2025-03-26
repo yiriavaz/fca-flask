7d39bb318b7cd9427ec50a7ebc01d2f31606225e
yiriavaz <yiria.vaz@gmail.com>
2025-03-26 08:57:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Yiria Vaz":"5610599991", "Josue Perez" : "55555555555", "Jaime Rodriguez" : "1234567823", "James Bond" : "12345678909"})
-
+    cris = Contacto("Cristian", "56565656560")
+    lolita = Contacto("Lolita Cortez", "5656565656")
+    iron = Contacto("Iron man", "15656565657")
+    list = []
+    list.append(cris)
+    list.append(lolita)
+    list.append(iron)
+    return jsonify(results = list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
